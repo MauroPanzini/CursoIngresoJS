21dd627e5347f0711235823e1432f74e189b8931
MauroPanzini <mauropanzini@MacBook-Air.local>
2022-02-04 01:14:10 -0300

    TP E/S-3 terminado

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,14 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+	var fahrenheit = parseInt(txtIdTemperatura.value);
+    var conversion = (fahrenheit - 32) *5/9;
+    alert("La temperatura es de "+ conversion + "ºC");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    var centrigrados = parseInt(txtIdTemperatura.value);
+    var conversion = (centrigrados * 9/5)+32;
+    alert("La temperatura es de "+ conversion + "ºF");
 }
