e70e1d0c83ef98194bfb581bed3c7850b28cc818
MauroPanzini <mauropanzini@MacBook-Air.local>
2022-02-04 00:36:31 -0300

    TP 1 terminado

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,28 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno = parseInt(txtIdPrecioUno.value);
+    var precioDos = parseInt(txtIdPrecioDos.value);
+    var precioTres = parseInt(txtIdPrecioTres.value);
+    var suma = precioUno + precioDos + precioTres;
+
+    alert("La suma da un total de: " + suma);
 }
 function Promedio () 
 {
-	
+	var precioUno = parseInt(txtIdPrecioUno.value);
+    var precioDos = parseInt(txtIdPrecioDos.value);
+    var precioTres = parseInt(txtIdPrecioTres.value);
+    var suma = precioUno + precioDos + precioTres;
+    var promedio = suma/3;
+    alert("El promedio es: " + promedio);
 }
 function PrecioFinal () 
 {
-	
+    var precioUno = parseInt(txtIdPrecioUno.value);
+    var precioDos = parseInt(txtIdPrecioDos.value);
+    var precioTres = parseInt(txtIdPrecioTres.value);
+    var suma = precioUno + precioDos + precioTres;
+    var masIva = suma*21/100 + suma;
+    alert("El precio final es de: "+ masIva);	
 }
