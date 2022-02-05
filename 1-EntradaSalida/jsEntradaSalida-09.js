3198242d620bb51c63a761a3a6850573d7bb109e
MauroPanzini <mauropanzini@MacBook-Air.local>
2022-02-04 23:14:35 -0300

    Agregados bis 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,7 +5,8 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var porcentaje = prompt();
 	var sueldo = parseInt(txtIdSueldo.value);
-	var aumento = sueldo/10 + sueldo;
+	var aumento = sueldo*porcentaje/100 + sueldo;
 	txtIdResultado.value = aumento;	
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,7 +5,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	var porcentaje = prompt();
 	var importe = parseInt(txtIdImporte.value);
-	var descuento = importe - importe*25/100;
+	var descuento = importe - importe*porcentaje/100;
 	txtIdResultado.value = descuento;	
+	alert("El importe es " + importe + ", el descuento es de un " + porcentaje + "%, y el importe con el descuento es " + descuento);
 }
