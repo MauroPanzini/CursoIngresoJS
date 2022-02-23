db0280167ff8ba8811886adf6cc2a15285102bf3
Mauro Panzini <mauropanzini18@gmail.com>
2022-02-23 16:05:15 -0300

    Update jsIteracionesWhile-07.js
    Ej 7 terminado

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -11,6 +11,13 @@ function mostrar()
 	acumulador=0;
 	respuesta='si';
 
+	while(respuesta == "si"){
+		numeroIngresado = parseInt(prompt("Ingrese número/s. Ya ingresaste " + contador + " números: "));
+		acumulador += numeroIngresado;
+		contador++;
+		respuesta = prompt("¿Desea seguir ingresando números?").toLowerCase();
+	}
+
 
 	txtIdSuma.value=acumulador;
 	txtIdPromedio.value=acumulador/contador;
